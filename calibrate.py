128a885f785c443eea4a83fb89c9aab1945e8eae
Bill Zito <bill.r.zito@gmail.com>
2017-01-27 14:50:34 -0800

    Undistort images given calibration

--- calibrate.py
+++ calibrate.py
@@ -10,9 +10,9 @@ import matplotlib.pyplot as plt
 
 def calibrate_cam():
   # set object points to 0's to start
-  objp = np.zeros((6*8, 3), np.float32)
+  objp = np.zeros((6*9, 3), np.float32)
   # set objp to the coordinates of the grid
-  objp[:,:2] = np.mgrid[0:8, 0:6].T.reshape(-1, 2)
+  objp[:,:2] = np.mgrid[0:9, 0:6].T.reshape(-1, 2)
   # print('objp .shape', objp[9])
 
   #store the the object points and image points
@@ -33,7 +33,7 @@ def calibrate_cam():
     #get the corners 
     #ret is boolean true false if found corners
     ret, corners = cv2.findChessboardCorners(gray, (9,6), None)
-    print('ret is', ret, 'at idx', idx, 'img name', fname)
+    # print('ret is', ret, 'at idx', idx, 'img name', fname)
     #if found, add object and image points
     if ret == True:
       found_count += 1
@@ -47,37 +47,36 @@ def calibrate_cam():
       cv2.imshow('img', img)
       cv2.waitKey(500)
 
-  print('img count is', img_count)
-  print('found count is', found_count)
+  # print('img count is', img_count)
+  # print('found count is', found_count)
   cv2.destroyAllWindows()
 
+  print('objpoints shape', len(objpoints[0]))
+  print('imgpoints shape', len(imgpoints[0]))
   return objpoints, imgpoints
 
 
-def undistort(objpoints, imgpoints):
+def undist(objpoints, imgpoints):
   img = cv2.imread('test_images/test1.jpg')
   img_size = (img.shape[1], img.shape[0])
 
   #does it return, what is the distance, what are the r and t vals
   ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, img_size, None, None)
-
   #undistort it
   dst = cv2.undistort(img, mtx, dist, None, mtx)
   cv2.imwrite('test_images/test1_undist.jpg', dst)
-
   dist_pickle = {}
   dist_pickle["mtx"] = mtx
   dist_pickle["dist"] = dist
   pickle.dump( dist_pickle, open("test_dist_pickle.p", "wb"))
-
-  f, (ax1, ax2) = plt.subplots(1, 2, figsize=(20, 10))
-  ax1.imshow(img)
-  ax1.set_title('Original Image', fontsize=30)
-  ax2.imshow(dst)
-  ax2.set_title('Undistoredted Image', fontsize=30)
+  # f, (ax1, ax2) = plt.subplots(1, 2, figsize=(20, 10))
+  # ax1.imshow(img)
+  # ax1.set_title('Original Image', fontsize=30)
+  # ax2.imshow(dst)
+  # ax2.set_title('Undistoredted Image', fontsize=30)
 
 if __name__ == '__main__':
   objpoints, imgpoints = calibrate_cam()
   print('calibration complete')
-  # undistort(objpoints, imgpoints)
+  undist(objpoints, imgpoints)
   print('undistort complete')
