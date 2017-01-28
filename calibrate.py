0eb32ee7554cbbfc7dd82dcdba7b26e2d255ca0b
Bill Zito <bill.r.zito@gmail.com>
2017-01-28 15:27:26 -0800

    Automate undistortion of all test images
    Change perspective

--- calibrate.py
+++ calibrate.py
@@ -2,6 +2,7 @@
 calibrate.py takes 20 chessboard images and uses them to
 calibrate the camera. It can then undistort a test image
 '''
+import os
 import cv2
 import glob
 import pickle
@@ -9,10 +10,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def calibrate_cam():
+  nx = 9
+  ny = 6
   # set object points to 0's to start
-  objp = np.zeros((6*9, 3), np.float32)
+  objp = np.zeros((ny*nx, 3), np.float32)
   # set objp to the coordinates of the grid
-  objp[:,:2] = np.mgrid[0:9, 0:6].T.reshape(-1, 2)
+  objp[:,:2] = np.mgrid[0:nx, 0:ny].T.reshape(-1, 2)
   # print('objp .shape', objp[9])
 
   #store the the object points and image points
@@ -32,7 +35,7 @@ def calibrate_cam():
 
     #get the corners 
     #ret is boolean true false if found corners
-    ret, corners = cv2.findChessboardCorners(gray, (9,6), None)
+    ret, corners = cv2.findChessboardCorners(gray, (nx, ny), None)
     # print('ret is', ret, 'at idx', idx, 'img name', fname)
     #if found, add object and image points
     if ret == True:
@@ -42,7 +45,7 @@ def calibrate_cam():
 
       #draw and display the corners
       #8 by 6 corners. draw  
-      cv2.drawChessboardCorners(img, (9, 6), corners, ret)
+      cv2.drawChessboardCorners(img, (nx, ny), corners, ret)
       # show the image
       cv2.imshow('img', img)
       cv2.waitKey(500)
@@ -51,29 +54,45 @@ def calibrate_cam():
   # print('found count is', found_count)
   cv2.destroyAllWindows()
 
-  print('objpoints shape', len(objpoints[0]))
-  print('imgpoints shape', len(imgpoints[0]))
   return objpoints, imgpoints
 
 
 def undist(objpoints, imgpoints):
+  
   img = cv2.imread('test_images/test1.jpg')
   img_size = (img.shape[1], img.shape[0])
 
   #does it return, what is the distance, what are the r and t vals
   ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, img_size, None, None)
-  #undistort it
-  dst = cv2.undistort(img, mtx, dist, None, mtx)
-  cv2.imwrite('test_images/test1_undist.jpg', dst)
+  
+  test_list = os.listdir('test_images')
+
+  #undistort all
+  for index, img_name in enumerate(test_list):
+    if img_name.startswith('test'):
+      print('img name is', img_name)
+
+      img = cv2.imread('test_images/' + img_name)
+      dst = cv2.undistort(img, mtx, dist, None, mtx)
+      cv2.imwrite('output_images/' + img_name + '_undistorted.jpg', dst)
+
   dist_pickle = {}
   dist_pickle["mtx"] = mtx
   dist_pickle["dist"] = dist
   pickle.dump( dist_pickle, open("test_dist_pickle.p", "wb"))
+  #show images
   # f, (ax1, ax2) = plt.subplots(1, 2, figsize=(20, 10))
   # ax1.imshow(img)
   # ax1.set_title('Original Image', fontsize=30)
   # ax2.imshow(dst)
   # ax2.set_title('Undistoredted Image', fontsize=30)
+
+# def changePerspective(img):
+   # src = np.float32([corner1, corner2, corner3, corner4])
+   # dst = np.float32('')
+   # M = cv2.getPerspectiveTransform(src, dst)
+   # shape = img.shape
+   # warped = cv2.warpPerspective(img, M, (shape[1], shape[0]))
 
 if __name__ == '__main__':
   objpoints, imgpoints = calibrate_cam()
