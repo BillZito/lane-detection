054e8c47116baa840d155d9d604b4bb9938cdd9c
Bill Zito <bill.r.zito@gmail.com>
2017-01-29 20:29:48 -0800

    Calculate curve of lines given fake random data

--- threshold.py
+++ threshold.py
@@ -82,57 +82,54 @@ def hls_thresh(img, thresh=(0, 255)):
 combine the thresholding functions
 '''
 def combo_thresh():
-  x_thresholded = abs_sobel_thresh(image, orient='x', sobel_kernel=3, thresh=(6, 120))
-  plt.imshow(x_thresholded, cmap='gray')
-  plt.title('xthresh')
+  x_thresholded = abs_sobel_thresh(image, orient='x', sobel_kernel=3, thresh=(10, 120))
+  # plt.imshow(x_thresholded, cmap='gray')
+  # plt.title('xthresh')
   # plt.show()
 
-  y_thresholded = abs_sobel_thresh(image, orient='y', sobel_kernel=3, thresh=(10, 100))
-  plt.imshow(y_thresholded, cmap='gray')
-  plt.title('ythresh')
+  y_thresholded = abs_sobel_thresh(image, orient='y', sobel_kernel=3, thresh=(15, 100))
+  # plt.imshow(y_thresholded, cmap='gray')
+  # plt.title('ythresh')
   # plt.show()
 
   binary_output = np.zeros_like(x_thresholded)
   # using bitwise or + and, look up how working
   binary_output[((x_thresholded == 1) & (y_thresholded == 1))] = 1
-  plt.imshow(binary_output, cmap='gray')
-  plt.title('x and y')
+  # plt.imshow(binary_output, cmap='gray')
+  # plt.title('x and y')
   # plt.show()
 
   hls_thresholded = hls_thresh(image, thresh=(90, 255))
-  plt.imshow(hls_thresholded, cmap='gray')
-  plt.title('hls')
+  # plt.imshow(hls_thresholded, cmap='gray')
+  # plt.title('hls')
   # plt.show()
   
 
-  binary_output = np.zeros_like(x_thresholded)
-  # using bitwise or + and, look up how working
-  binary_output[((x_thresholded == 1) & (y_thresholded == 1)) & (hls_thresholded == 1)] = 1
-  plt.imshow(binary_output, cmap='gray')
-  plt.title('x and y, and hls')
+  # binary_output = np.zeros_like(x_thresholded)
+  # # using bitwise or + and, look up how working
+  # binary_output[((x_thresholded == 1) & (y_thresholded == 1)) & (hls_thresholded == 1)] = 1
+  # plt.imshow(binary_output, cmap='gray')
+  # plt.title('x and y, and hls')
   # plt.show()
-  # 
 
   mag_thresholded = mag_thresh(image, sobel_kernel=3, mag_thresh=(20, 160))
-  plt.imshow(mag_thresholded, cmap='gray')
-  plt.title('magnitude')
+  # plt.imshow(mag_thresholded, cmap='gray')
+  # plt.title('magnitude')
   # plt.show()
 
   dir_thresholded = dir_thresh(image, sobel_kernel=15, thresh=(.7, 1.2))  
-  plt.imshow(dir_thresholded, cmap='gray')  
-  plt.title('directional')
+  # plt.imshow(dir_thresholded, cmap='gray')  
+  # plt.title('directional')
   # plt.show()
 
   binary_output = np.zeros_like(dir_thresholded)
-  # using bitwise or + and, look up how working
   binary_output[((dir_thresholded == 1) & (mag_thresholded == 1))] = 1
-  plt.imshow(binary_output, cmap='gray')
-  plt.title('dir and mag')
+  # plt.imshow(binary_output, cmap='gray')
+  # plt.title('dir and mag')
   # plt.show()
 
 
   binary_output = np.zeros_like(dir_thresholded)
-  # using bitwise or + and, look up how working
   binary_output[((x_thresholded == 1) & (y_thresholded == 1)) | ((dir_thresholded == 1) & (mag_thresholded == 1) & (hls_thresholded == 1))] = 1
   # 
   return binary_output
@@ -168,24 +165,81 @@ def changePerspective(img):
   warped = cv2.warpPerspective(img, M, (shape[1], shape[0]))
   return warped
 
+'''
+get the left and right images
+'''
+def get_lr():
+  # -divide vertically by 8 (720/8 is 90)
+  # -find peak between 200/ and 500/ 
+  # -use a 50 pixel wide map
+  # -add to array
+  # -fit line to those pixels
+  print('hello world')
+
+'''
+calculate the curve of the lines based on the pixels
+'''
+def calc_curve():
+  #replace the y and x data with my data and this code should work...
+  #make fake y-range data
+  yvals = np.linspace(0, 100, num=101)*7.2
+  #y-range as image... what does that mean?
+  leftx = np.array([200 + (elem**2)*4e-4 + np.random.randint(-50, high=51) for idx, elem in enumerate(yvals)])
+  #reverse to match top-to-bottom in y (because np images reversed?)
+  leftx = leftx[::-1]
+  #gen right images
+  rightx = np.array([900 + (elem**2)*4e-4 + np.random.randint(-50, high=51) for idx, elem in enumerate(yvals)])
+
+  rightx = rightx[::-1]
+
+  #fit to second order polynomial
+  left_fit = np.polyfit(yvals, leftx, 2)
+  left_fitx = left_fit[0]*yvals**2 + left_fit[1]*yvals + left_fit[2]
+  right_fit = np.polyfit(yvals, rightx, 2)
+  right_fitx = right_fit[0]*yvals**2 + right_fit[1]*yvals + right_fit[2]
+
+  plt.plot(leftx, yvals, 'o', color='red')
+  plt.plot(rightx, yvals, 'o', color='blue')
+  plt.xlim(0, 1280)
+  plt.ylim(0, 720)
+  plt.plot(left_fitx, yvals, color='green', linewidth=3)
+  plt.plot(right_fitx, yvals, color='green', linewidth=3)
+  plt.gca().invert_yaxis()
+  plt.show()
 
 if __name__ == '__main__':
+  calc_curve()
   # image = mpimg.imread('straight_road_1x.jpg')
-  image = mpimg.imread('output_images/test1_undistorted.jpg')
-  plt.imshow(image)
-  plt.title('starter')
-  plt.show()
-
-  warped = changePerspective(image)
-  plt.imshow(warped)
-  plt.title('warped')
-  plt.show()
-
-  # x_thresholded = abs_sobel_thresh(image, orient='x', sobel_kernel=3, thresh=(6, 120))
-  # plt.imshow(x_thresholded, cmap='gray')
+  # image = mpimg.imread('output_images/test5_undistorted.jpg')
+  # plt.imshow(image)
+  # plt.title('starter')
   # plt.show()
 
-  # y_thresholded = abs_sobel_thresh(image, orient='y', sobel_kernel=3, thresh=(10, 100))
+  # combo_image = combo_thresh()
+  # plt.imshow(combo_image, cmap='gray')
+  # plt.title('combo_image')
+  # plt.show()
+
+  # warped_image = changePerspective(combo_image)
+  # plt.imshow(warped_image, cmap='gray')
+  # plt.title('warped_image')
+  # plt.show()
+  # print('warped shape', warped_image.shape)
+  # print('warped shape[0]/2', int(warped_image.shape[0]/2))
+
+  # histogram = np.sum(warped_image[int(warped_image.shape[0]/8):, :], axis=0)
+  # plt.plot(histogram)
+  # plt.title('histogram')
+  # plt.show()
+  # x_thresholded = abs_sobel_thresh(image, orient='x', sobel_kernel=3, thresh=(10, 120))
+  # plt.imshow(x_thresholded, cmap='gray')
+  # plt.title('xthresh')
+  # plt.show()
+
+
+
+
+  # y_thresholded = abs_sobel_thresh(image, orient='y', sobel_kernel=3, thresh=(15, 100))
   # plt.imshow(y_thresholded, cmap='gray')
   # plt.title('ythresh')
   # plt.show()
@@ -195,7 +249,7 @@ if __name__ == '__main__':
   # plt.title('hls')
   # plt.show()
 
-  # mag_thresholded = mag_thresh(image, sobel_kernel=3, mag_thresh=(20, 160))
+  # mag_thresholded = mag_thresh(image, sobel_kernel=3, mag_thresh=(20, 100))
   # plt.imshow(mag_thresholded, cmap='gray')
   # plt.title('magnitude')
   # plt.show()
@@ -203,9 +257,4 @@ if __name__ == '__main__':
   # dir_thresholded = dir_thresh(image, sobel_kernel=15, thresh=(.7, 1.2))  
   # plt.imshow(dir_thresholded, cmap='gray')  
   # plt.title('directional')
-  # plt.show()
-
-  # combo = combo_thresh()
-  # plt.imshow(combo, cmap='gray')
-  # plt.title('combo')
   # plt.show()
